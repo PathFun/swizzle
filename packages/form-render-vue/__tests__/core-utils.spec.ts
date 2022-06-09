fbc279cfdf7c5381f21ceae10770624f371104e9
tangql <360603837@qq.com>
2022-06-09 14:11:19 +0800

    eslint

---
+++ packages/form-render-vue/__tests__/core-utils.spec.ts
@@ -0,0 +1,25 @@
+import { flattenSchema } from '../src/utils';
+describe('Test FormRender Utils', () => {
+  it('Test flattenSchema', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        input1: {
+          title: '简单输入框',
+          type: 'string',
+          order: 2,
+          required: true,
+        },
+        select1: {
+          title: '单选',
+          type: 'string',
+          order: 1,
+          enum: ['a', 'b', 'c'],
+          enumNames: ['早', '中', '晚'],
+        },
+      },
+    };
+    const _schema = flattenSchema(schema, '#', null, {});
+    expect(Object.keys(_schema)).toEqual(['select1', 'input1', '#']);
+  });
+});
